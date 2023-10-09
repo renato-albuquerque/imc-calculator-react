367c261a4ae27a4176146024b60d5766c2012e47
Renato Albuquerque <renatomalb83@gmail.com>
2023-10-09 00:00:33 -0300

    refactoring, main page, english to portuguese

--- imc_calculator/src/components/ImcCalc.jsx
+++ imc_calculator/src/components/ImcCalc.jsx
@@ -28,27 +28,27 @@ const ImcCalc = ({ calcImc }) => {
 
   return (
     <div id="calc-container">
-        <h2>IMC Calculator</h2>
+        <h2>Calculadora de IMC</h2>
         <form id="imc-form">
             <div className="form-inputs">
                 <div className="form-control">
-                    <label htmlFor="height">Height:</label>
+                    <label htmlFor="height">Altura:</label>
                     <input 
                         type="text" 
                         name="height" 
                         id="height" 
-                        placeholder="Example 1,75"
+                        placeholder="Exemplo 1,75"
                         onChange={(e) => handleHeightChange(e)}
                         value={height}
                     />
                 </div>
                 <div className="form-control">
-                    <label htmlFor="weight">Weight:</label>
+                    <label htmlFor="weight">Peso:</label>
                     <input 
                         type="text" 
                         name="weight" 
                         id="weight" 
-                        placeholder="Example 70,5"
+                        placeholder="Exemplo 70,5"
                         onChange={(e) => handleWeightChange(e)} 
                         value={weight}
                     />
@@ -57,9 +57,9 @@ const ImcCalc = ({ calcImc }) => {
             <div className="action-control">
                 <Button 
                   id="calc-btn" 
-                  text="Calculate" 
+                  text="Calcular" 
                   action={(e) => calcImc(e, height, weight)} />
-                <Button id="clear-btn" text="Clear" action={clearForm} />
+                <Button id="clear-btn" text="Limpar" action={clearForm} />
             </div>
         </form>
     </div>
